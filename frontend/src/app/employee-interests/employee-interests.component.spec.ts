5d829852448c88ba727bbaf48af41ebc85c1a880
Alasdair Hall <alasdair.hall@zuhlke.com>
2019-03-21 16:42:19 +0100

    Did some async tests somehow

--- frontend/src/app/employee-interests/employee-interests.component.spec.ts
+++ frontend/src/app/employee-interests/employee-interests.component.spec.ts
@@ -1,23 +1,32 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, tick} from '@angular/core/testing';
 
 import {EmployeeInterestsComponent} from './employee-interests.component';
-import {Interest} from '../domain/Interest';
+import {Interest} from "../domain/Interest";
+import {EmployeeService} from "../employee.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {Employee} from "../domain/Employee";
+import {Observable, of} from "rxjs";
 
 describe('EmployeeInterestsComponent', () => {
   let component: EmployeeInterestsComponent;
   let fixture: ComponentFixture<EmployeeInterestsComponent>;
-  const interests: Interest[] = [{name: 'Testing', id: 0}, {name: 'iOS', id: 1}];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [EmployeeInterestsComponent]
-    }).compileComponents();
+      imports: [
+        RouterTestingModule,
+      ],
+      declarations: [EmployeeInterestsComponent],
+      providers: [
+        {provide: EmployeeService, useClass: EmployeeServiceStub}
+      ]
+    }).compileComponents()
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeInterestsComponent);
     component = fixture.componentInstance;
-    component.employee = {firstName: 'Tommy', lastName: 'Tester', id: 1, code: 'tote', interests: interests};
     fixture.detectChanges();
   });
 
@@ -25,15 +34,39 @@ describe('EmployeeInterestsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('displays employees name in a h1', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Tommy Tester');
+  it("displays employee's name in a h1",  () => {
+    async(() => {
+      component.ngOnInit();
+      fixture.detectChanges();
+      tick(400);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('h1').textContent).toBe('Tommy Tester');
+    })
+
   });
 
-  it('displays a list populated with the employees interests', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    let listResult = compiled.querySelectorAll('#interests-list li');
-    expect(listResult.length).toEqual(interests.length);
-    // expect(compiled.querySelector('li').
+  it("displays a list populated with the employee's interests", () => {
+    async(() => {
+      component.ngOnInit();
+      fixture.detectChanges();
+      tick(400);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      let listResult = compiled.querySelectorAll('#interests-list li');
+      expect(listResult.length).toEqual(INTERESTS.length)
+    })
   })
 });
+
+const INTERESTS: Interest[] = [{name: "Testing", id: 0}, {name: "iOS", id: 1}];
+const EMPLOYEES: Employee[] = [
+  {firstName: 'Tommy', lastName: 'Tester', id: 1, code: 'tote', interests: INTERESTS}
+];
+
+class EmployeeServiceStub {
+  getEmployee(code: string): Observable<Employee> {
+    return of(EMPLOYEES.find(e => e.code === code));
+  }
+}
