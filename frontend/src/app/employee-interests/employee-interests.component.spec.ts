faa9eccff5c4f4beb5f710d4b2266f099805b368
sawh <sam.white@zuhlke.com>
2019-03-21 16:30:50 +0100

    Corrected some lint error

--- frontend/src/app/employee-interests/employee-interests.component.spec.ts
+++ frontend/src/app/employee-interests/employee-interests.component.spec.ts
@@ -1,12 +1,12 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {EmployeeInterestsComponent} from './employee-interests.component';
-import {Interest} from "../domain/Interest";
+import {Interest} from '../domain/Interest';
 
 describe('EmployeeInterestsComponent', () => {
   let component: EmployeeInterestsComponent;
   let fixture: ComponentFixture<EmployeeInterestsComponent>;
-  let interests: Interest[] = [{name: "Testing", id: 0}, {name: "iOS", id: 1}];
+  const interests: Interest[] = [{name: 'Testing', id: 0}, {name: 'iOS', id: 1}];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,15 +25,15 @@ describe('EmployeeInterestsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("displays employee's name in a h1", () => {
+  it('displays employees name in a h1', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Tommy Tester');
-  })
+  });
 
-  it("displays a list populated with the employee's interests", () => {
+  it('displays a list populated with the employees interests', () => {
     const compiled = fixture.debugElement.nativeElement;
-    let listResult = compiled.querySelectorAll('#interests-list li')
-    expect(listResult.length).toEqual(interests.length)
-    //expect(compiled.querySelector('li').
+    let listResult = compiled.querySelectorAll('#interests-list li');
+    expect(listResult.length).toEqual(interests.length);
+    // expect(compiled.querySelector('li').
   })
 });
